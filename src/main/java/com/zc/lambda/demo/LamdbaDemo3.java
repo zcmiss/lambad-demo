c3c6830c60a184c78d4329ff45291d723a471dc8
zengc <zengchao405@163.com>
2024-01-17 23:11:37 +0800

    lamdba 简化规则

---
+++ src/main/java/com/zc/lambda/demo/LamdbaDemo3.java
@@ -0,0 +1,40 @@
+package com.zc.lambda.demo;
+
+import java.util.function.IntConsumer;
+
+/**
+ * lamdba demo3
+ *
+ * @author zengc
+ * @date 2024/01/17
+ */
+public class LamdbaDemo3 {
+    /**
+     * 主要
+     *
+     * @param args args
+     */
+    public static void main(String[] args) {
+        foreacharr(new IntConsumer() {
+            @Override
+            public void accept(int value) {
+                System.out.printf("%d ", value);
+            }
+        });
+        System.out.println();
+        foreacharr(System.out::print);
+    }
+
+    /**
+     * 前景
+     *
+     * @param intConsumer int消费者
+     */
+    public static void foreacharr(IntConsumer intConsumer) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        for (int i : arr) {
+            intConsumer.accept(i);
+        }
+    }
+
+}
